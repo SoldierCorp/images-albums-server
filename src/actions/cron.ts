11c16909a27a7ba6f4ccba78e6dab6a60feec41e
aldo-rl <aldorl.mee@gmail.com>
2022-11-17 12:11:23 -0600

    DELETE: remove comments to cronjob

--- src/actions/cron.ts
+++ src/actions/cron.ts
@@ -1,11 +1,10 @@
 import cron from 'node-cron'
-
-// import { saveTwetterImages } from '../getAllTweets'
+import { saveTwetterImages } from '../getAllTweets'
 
 const runCronjob = async () => {
   console.log('running a At 07:00.');
-  // const images = await saveTwetterImages()
-  // console.log('total images uploaded: ', images.length);
+  const images = await saveTwetterImages()
+  console.log('total images uploaded: ', images.length);
 }
 
 const saveTwetterImagesCronjob = async () => {
