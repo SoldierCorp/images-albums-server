bd1e479382e9f296ac47bb916995c00c812e5287
aldo-rl <aldorl.mee@gmail.com>
2022-11-17 13:40:23 -0600

    UPDATE: display status actions in console

--- src/getAllTweets/index.ts
+++ src/getAllTweets/index.ts
@@ -22,43 +22,44 @@ const saveTweeterImages = async (): Promise<Image[]> => {
   const username = 'ImagesAlbum'
 
   const user = await getUser(username)
+  console.log('---------------------------')
   console.log('user: ', user)
 
-  console.log('---------------------------')
-
   const lastImagesSaved = await getLastImages(username)
+  console.log('---------------------------')
   console.log('lastImagesSaved: ', lastImagesSaved.length)
 
   const isFirstExecution = lastImagesSaved.length === 0
+  console.log('---------------------------')
   console.log('isFirstExecution: ', isFirstExecution)
 
   const tweetsImagesAll = isFirstExecution
     ? await getAllTweets(user.id)
     : await getTweets(user.id)
-  console.log('tweetsImagesAll: ', tweetsImagesAll.length)
 
   console.log('---------------------------')
+  console.log('tweetsImagesAll: ', tweetsImagesAll.length)
 
   const imagestoSave = await filterImagesToSave(username, tweetsImagesAll)
+  console.log('---------------------------')
   console.log('images to save: ', imagestoSave.length)
 
   console.log('---------------------------')
-
-  console.log('Uploading images...')
+  console.log('Uploading images in cloudinary ...')
   const imageUploaded = await uploadMultipleImages(imagestoSave)
   console.log('Images uploaded: ', imageUploaded.length)
 
   console.log('---------------------------')
-
+  console.log('Adding tags to images from immaga ...')
   const imagesWithTags = await addTagsToImages(imageUploaded)
 
+  console.log('---------------------------')
+  console.log('Inserting images in firebase ...')
   await insertImages(username, imagesWithTags)
   console.log('Images saved in db: ', imagesWithTags.length)
 
   const orderedImages = orderDesc(imagesWithTags)
-
   return orderedImages
-
 }
 
 
