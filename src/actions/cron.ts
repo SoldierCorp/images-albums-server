0742b330bb0e0c191474a2685ed6ff7cd28915fa
aldo-rl <aldorl.mee@gmail.com>
2022-11-18 09:53:22 -0600

    ADD: Execution time and display tweetId images uploaded

--- src/actions/cron.ts
+++ src/actions/cron.ts
@@ -4,13 +4,26 @@ import { saveTweeterImages } from '../getAllTweets'
 const runCronjob = async () => {
   console.log('Running At 07:00.');
   const images = await saveTweeterImages()
+  return images
+}
+
+const runCronjobExecutionTime = async () => {
+  const start = Date.now();
+  const images = await runCronjob()
+  const imagesUploaded = images.map(image => image.tweetId)
+  console.log('---------------------------')
   console.log('Total images uploaded: ', images.length);
+  console.log("Id's images uploaded: ", imagesUploaded);
+
+  const end = Date.now();
+  const executionTime = `Execution time: [${end - start} ms]`
+  console.log(executionTime)
 }
 
 const saveTweeterImagesCronjob = async () => {
-  await runCronjob()
+  await runCronjobExecutionTime()
   cron.schedule('0 7 * * *', async () => {
-    await runCronjob()
+    await runCronjobExecutionTime()
   }).start();
 }
 
